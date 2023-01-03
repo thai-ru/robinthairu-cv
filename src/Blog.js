47af3d9aa06305dc60f6e4821260e5b9ca81513d
Robinson Thairu <robinthairu34@gmail.com>
2023-01-03 13:10:26 +0300

    React catch fetch error with Json server on Blog List

--- src/Blog.js
+++ src/Blog.js
@@ -1,21 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const BlogPage = () =>{
-    const [blogs, setblogs] = useState ([
+    const [blogs, setblogs] = useState (null/*[
         {title:"Why linux", body:"Linux is everywhere, used by ....",author:"Thairu Robin", id:1},
         {title:"What is ChatGpt", body:"What is ChatGpt and why ....",author:"RT Ndung'u", id:2},
         {title:"Computer Processor", body:"What are these ....",author:"Nj ndungu", id:3},
-    ]);
+    ]*/);
 
-    const handleDelete = (id) => {
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    /*const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setblogs(newBlogs);
-    }
+    }*/
+
+    
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then( res => {
+            if(!res.ok){
+                throw Error('Could not fetch data');
+
+            }
+            return res.json();
+        })
+        .then(data => {
+            setblogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+            
+        })
+    }, []);
 
     return(
             <div className="bg-gray-200">
-                <BlogList blogs={blogs} title="Blogs!" handleDelete = {handleDelete}/>
+            { error && <div>Could not fetch blogss..</div>}
+            {isPending && <div>Loading Blogs....</div>}
+               {blogs && <BlogList blogs={blogs} title="Blogs!"/>}
+               {/* <BlogList blogs={blogs.filter(() =>blogs.author === "RT Ndung'u")} title ="Thairu's blog"/>*/}
             </div>
         );
 }
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,18 @@
 const BlogList = (props) =>{
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
+    //const handleDelete = props.handleDelete;
 
     return(
         <div className="bg-gray-200">
         <h2 className="text-3xl font-bold text-center">{ title }</h2>
             {blogs.map((blog) => (
                 
-                <div className='m-1' key={blog.id} >
+                <div className='m-1 ml-4' key={blog.id} >
                     <h2 className='text-2xl font-semibold text-pink-900 p-2' >{ blog.title }</h2>
                     <p className='text-md font-thin text-pink-800 ml-2' >Written by { blog.author }</p>
 
-                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+                    {/*<button onClick={() => handleDelete(blog.id)}>Delete blog</button>*/}
 
                 </div>
             ))}
